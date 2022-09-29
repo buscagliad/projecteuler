a73a8410b12e1a092f3e514141b93e2902924db0
David Buscaglia <david.buscaglia@vts-i.com>
2022-09-29 07:23:35 -0700

    Problem 118 solved: 44680

--- 118/panprimesets.cc
+++ 118/panprimesets.cc
@@ -37,7 +37,7 @@ int	getplist(vector<long> &list, int n)	// get list of n-digit non-repeating dig
 {
 	long minp = xpower(10, n-1);
 	long maxp = 10 * minp;
-	printf("n: %d  Min: %ld  Max: %ld\n", n, minp, maxp);
+	//printf("n: %d  Min: %ld  Max: %ld\n", n, minp, maxp);
 	Prime	p(1l);
 	long	np = p.next();
 	while(np < minp) np = p.next();
@@ -49,7 +49,7 @@ int	getplist(vector<long> &list, int n)	// get list of n-digit non-repeating dig
 		}
 		np = p.next();
 	} while (np < maxp);
-	printf("Found %lu - %d digit non-repeating primes\n", list.size(), n);
+	//printf("Found %lu - %d digit non-repeating primes\n", list.size(), n);
 	return list.size();
 }
 
@@ -175,7 +175,7 @@ long do5(vlt &a, vlt &b, long bmult, vlt &c, long cmult, vlt &d, long dmult, vlt
 						num = ( ( (*ai * bmult + *bi) * cmult + *ci ) * dmult + *di) * emult + *ei;
 						if (isPandigital(num))
 						{
-							printf("%ld - Set: {%ld,%ld,%ld,%ld,%ld} is GOOD!\n", num, *ai, *bi, *ci, *di, *ei);
+							//printf("%ld - Set: {%ld,%ld,%ld,%ld,%ld} is GOOD!\n", num, *ai, *bi, *ci, *di, *ei);
 							count++;
 						}
 						else
@@ -216,7 +216,7 @@ long do6(vlt &a, vlt &b, long bmult, vlt &c, long cmult, vlt &d, long dmult, vlt
 							num = ( ( ( (*ai * bmult + *bi) * cmult + *ci ) * dmult + *di) * emult + *ei) * fmult + *fi;
 							if (isPandigital(num))
 							{
-								printf("%ld - Set: {%ld,%ld,%ld,%ld,%ld,%ld} is GOOD!\n", num, *ai, *bi, *ci, *di, *ei, *fi);
+								//printf("%ld - Set: {%ld,%ld,%ld,%ld,%ld,%ld} is GOOD!\n", num, *ai, *bi, *ci, *di, *ei, *fi);
 								count++;
 							}
 							else
@@ -236,7 +236,7 @@ long do6(vlt &a, vlt &b, long bmult, vlt &c, long cmult, vlt &d, long dmult, vlt
 int main()
 {
 	long e = eight();
-	printf("sets of 8 digits + 1 digit prime: %ld\n", e);
+	//printf("sets of 8 digits + 1 digit prime: %ld\n", e);
 	vector<long> p7, p6, p5, p4, p3, p2;
 	vector<long> p1 = {2, 3, 5, 7};
 	getplist(p2, 2);
@@ -245,31 +245,34 @@ int main()
 	getplist(p5, 5);
 	getplist(p6, 6);
 	getplist(p7, 7);
-	e += do2(p7, p2, 10000000);
-	e += do2(p6, p3, 1000000);
-	e += do2(p5, p4, 100000);
 	
-	e += do3(p6, p2, 100, p1, 10);
+	e += do2(p7, p2, 10000000);
 	e += do3(p7, p1, 10, p1, 10);
+	
+	e += do2(p6, p3, 1000000);
+	e += do3(p6, p2, 100, p1, 10);
+	e += do4(p6, p1, 10, p1, 10, p1, 10);
+
+	e += do2(p5, p4, 100000);
 	e += do3(p5, p3, 1000, p1, 10);
 	e += do3(p5, p2, 100, p2, 100);
+	e += do4(p5, p2, 100, p1, 10, p1, 10);
+	e += do5(p5, p1, 10, p1, 10, p1, 10, p1, 10);
+	
 	e += do3(p4, p4, 10000, p1, 10);
 	e += do3(p4, p3, 1000, p2, 100);
-	e += do3(p3, p3, 1000, p3, 1000);
-	
-	e += do4(p6, p1, 10, p1, 10, p1, 10);
 	e += do4(p4, p3, 1000, p1, 10, p1, 10);
 	e += do4(p4, p2, 100, p2, 100, p1, 10);
-	e += do4(p3, p3, 1000, p2, 100, p1, 10);
-	e += do4(p3, p2, 100, p2, 100, p2, 100);
-	
-	e += do5(p5, p1, 10, p1, 10, p1, 10, p1, 10);
 	e += do5(p4, p2, 100, p1, 10, p1, 10, p1, 10);
+
+	e += do3(p3, p3, 1000, p3, 1000);
+	e += do4(p3, p3, 1000, p2, 100, p1, 10);
 	e += do5(p3, p3, 1000, p1, 10, p1, 10, p1, 10);
+	e += do4(p3, p2, 100, p2, 100, p2, 100);
 	e += do5(p3, p2, 100, p2, 100, p1, 10, p1, 10);
-	e += do5(p2, p2, 100, p2, 100, p2, 100, p1, 10);
-	
 	e += do6(p3, p2, 100, p1, 10, p1, 10, p1, 10, p1, 10);
+	
+	e += do5(p2, p2, 100, p2, 100, p2, 100, p1, 10);
 	e += do6(p2, p2, 100, p2, 100, p1, 10, p1, 10, p1, 10);
 	printf("Total number of prime sets: %ld\n", e);
 }
