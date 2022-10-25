32faade5f51c8490afb6ac6600277c8b891fdac6
Dave Buscaglia <david.buscaglia@sbcglobal.net>
2022-10-25 09:43:12 -0700

    p131 added notes

--- 131/primecube.cc
+++ 131/primecube.cc
@@ -1,4 +1,22 @@
 
+/*
+There are some prime values, p, for which there exists a positive integer, n, such that the expression n^3 + n^2 p is a perfect cube.
+
+For example, when p = 19, 8^3 + 8^2×19 = 12^3.
+
+What is perhaps most surprising is that for each prime with this property the value of n is unique, and there are only four such primes below one-hundred.
+
+How many primes below one million have this remarkable property?
+* 
+* 
+* For this problem, we will note that if n = a^3, and p + n is a perfect cube, i.e. p + n = x^3,
+* then, 
+* 	n^3 + n^2 p = n^2 ( n + p ) = a^6 ( x^3 ) = ( a^2 x )^3
+* 
+* So, step one is to find n (which must be a perfect cube (a^3)) such that when added to p is a 
+* perfect cube.  (iscube tests for an integer being a perfect cube, and returns the cube root
+* if it is).  To limit the search, we can stop whenenver (n+1)^3 - n^3 > p.
+*/
 #include <cstdio>
 #include <cmath>
 #include "prime.h"
